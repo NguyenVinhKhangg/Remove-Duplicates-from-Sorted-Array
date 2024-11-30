fbe9f81ccd2a9881bfbb0aa52df6194ca0bbda42
NguyenVinhKhangg <126032433+NguyenVinhKhangg@users.noreply.github.com>
2024-11-30 16:46:15 +0700

    Add files via upload

---
+++ Ex02_DuplicateArray/DuplicateArrat/DuplicateArrat/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DuplicateArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+            int[] arr = { 1, 1, 2, 2, 3 };
+            Console.WriteLine(removeDuplicates.RemoveDuplicate(arr));
+        }
+    }
+}
---
+++ Ex02_DuplicateArray/DuplicateArrat/DuplicateArrat/RemoveDuplicates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateArray
+{
+    internal class RemoveDuplicates
+    {
+        public int RemoveDuplicate(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+            int j = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[j] != nums[i])
+                {
+                    nums[++j] = nums[i];
+                }
+            }
+
+            return j + 1;
+        }
+    }
+}
